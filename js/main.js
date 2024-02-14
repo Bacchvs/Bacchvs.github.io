46cd6bbc5de8de8bfa25f4db383fc67e0e27944d
Arnaud Sénécaut <senecaut.e2002261@etud.univ-ubs.fr>
2024-02-14 12:55:49 +0100

    affiche CV

--- js/main.js
+++ js/main.js
@@ -12,6 +12,6 @@ if (language === "fr"){
     CVname = "CV_Arnaud_SENECAUT_Biologie_EN.pdf"
 }
 
-document.getElementById("cv_displayer").setAttribute("src", "documents/CVname");
+document.getElementById("cv_displayer").setAttribute("src", "http://docs.google.com/gview?url=bacchvs.github.io/documents/"+CVname+"&embedded=true");
  
 
